18601469f110b1876254708550dc20ffa32bc3f9
Xiaohu Zhu <xzhu109@asu.edu>
2020-10-04 20:03:56 -0700

    Second commit

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,34 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history="0";
 
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
 
   public int getTotal () {
-    return 0;
+    return total;
   }
 
   public void add (int value) {
+	  total += value;
+	  history += " + "+value;
   }
 
   public void subtract (int value) {
+	  total-=value;
+	  history += " - "+value;
   }
 
   public String toString () {
-    return "";
+    return history;
   }
 
   public void clear() {
+	  history="0";
+	  total = 0;
+
   }
+
 }
